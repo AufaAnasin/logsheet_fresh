17740ac0ecc1cd1e52e19f9d435f689a883401d7
HelloCaptainAufa737 <anasinaufa@gmail.com>
2025-06-30 16:58:47 +0700

    [done] component graph

--- routes/web.php
+++ routes/web.php
@@ -12,37 +12,43 @@ Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
 
-Route::get('/dashboard', [DepartmentController::class, 'index'])
-    ->middleware(['auth', 'verified', 'non_operator'])
-    ->name('dashboard');
+Route::middleware(['auth', 'verified', 'non_operator'])->group(function () {
+    Route::get('/dashboard', [DepartmentController::class, 'index'])->name('dashboard');
+});
 
-Route::post('/departments', [DepartmentController::class, 'store'])->name('departments.store');
-Route::put('/departments/{department}', [DepartmentController::class, 'update'])->name('departments.update');
-Route::delete('/departments/{department}', [DepartmentController::class, 'destroy'])->name('departments.destroy');
+Route::middleware(['auth'])->group(function () {
+    Route::post('/departments', [DepartmentController::class, 'store'])->name('departments.store');
+    Route::put('/departments/{department}', [DepartmentController::class, 'update'])->name('departments.update');
+    Route::delete('/departments/{department}', [DepartmentController::class, 'destroy'])->name('departments.destroy');
 
-Route::post('/areas', [AreaController::class, 'store'])->name('areas.store');
-Route::put('/areas/{area}', [AreaController::class, 'update'])->name('areas.update');
-Route::delete('/areas/{area}', [AreaController::class, 'destroy'])->name('areas.destroy');
+    Route::post('/areas', [AreaController::class, 'store'])->name('areas.store');
+    Route::put('/areas/{area}', [AreaController::class, 'update'])->name('areas.update');
+    Route::delete('/areas/{area}', [AreaController::class, 'destroy'])->name('areas.destroy');
 
-Route::get('/components', function () {
-    return redirect()->route('dashboard');
-})->middleware('auth');
+    Route::get('/components', function () {
+        return redirect()->route('dashboard');
+    });
 
-Route::post('/components', [ComponentController::class, 'store'])->middleware('auth');
-Route::put('/components/{component}', [ComponentController::class, 'update'])->middleware('auth');
-Route::delete('/components/{component}', [ComponentController::class, 'destroy'])->middleware('auth');
+    Route::post('/components', [ComponentController::class, 'store']);
+    Route::put('/components/{component}', [ComponentController::class, 'update']);
+    Route::delete('/components/{component}', [ComponentController::class, 'destroy']);
 
-Route::get('/logdata', [ComponentController::class, 'logData'])->middleware('operator')->name('logdata');
-Route::post('/logdata/store', [ComponentController::class, 'storeLog'])->middleware('operator')->name('logdata.store');
+    Route::get('/analytics', [LogdataController::class, 'analytics'])->name('analytics');
+    Route::get('/analytics/components/{area}', [LogdataController::class, 'componentsInsights'])->name('components.insights');
+    Route::get('/userlist', [UserController::class, 'index'])->name('users.index');
+    Route::post('/userlist', [UserController::class, 'store'])->name('users.store');
+    Route::put('/userlist/{id}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/userlist/{id}', [UserController::class, 'destroy'])->name('users.destroy');
 
-Route::get('/graphdata', [ComponentController::class, 'graphData'])->middleware('operator')->name('graphdata');
-Route::get('/component-logs/{componentId}', [ComponentController::class, 'getComponentLogs'])->middleware('operator')->name('component.logs');
+    Route::get('/visualizeandtable/{componentId}', [LogdataController::class, 'visualizeandtable'])->name('visualizeandtable');
+});
 
+Route::middleware(['auth', 'operator'])->group(function () {
+    Route::get('/logdata', [ComponentController::class, 'logData'])->name('logdata');
+    Route::post('/logdata/store', [ComponentController::class, 'storeLog'])->name('logdata.store');
+    Route::get('/graphdata', [ComponentController::class, 'graphData'])->name('graphdata');
+    Route::get('/component-logs/{componentId}', [ComponentController::class, 'getComponentLogs'])->name('component.logs');
+});
 
-// Route::get('/user', [UserController::class, 'showUserList'])->middleware('auth')->name('user.list');
-Route::get('/userlist', [UserController::class, 'index'])->name('users.index');
-Route::post('/userlist', [UserController::class, 'store'])->name('users.store');
-Route::put('/userlist/{id}', [UserController::class, 'update'])->name('users.update');
-Route::delete('/userlist/{id}', [UserController::class, 'destroy'])->name('users.destroy');
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
